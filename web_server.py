bd1dc2984dd19b355f2f1e41e18e422095ab51eb
teerjustin <teerjustin@gmail.com>
2022-11-29 15:45:16 -0800

    refactored web_server

--- web_server.py
+++ web_server.py
@@ -4,26 +4,47 @@ import services.authentication.user_controller as uc
 
 app = Flask(__name__)
 
-USER = "master"
-PW = "password"
 
-@app.route("/homepage")
-def index():
-    print("here in home page")
-    return render_template('loginAndReg.html')
 
 @app.route("/login", methods = ["POST"])
 def login():
     email = request.json['email']
     password = request.json['password']
-    print("THIS IS MY USERS EMAIL: ", email)
-    print("THIS IS MY PASSWORD: ", password)
 
     u = uc.UserController()
 
-    print('hi')
     response = u.authenticate(email, password)
+    print("Attempted to authenticate User %s with status %s", email, response['status_code'])
     return response
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    u = uc.UserController()
+    email = request.json['email']
+    username = request.json['username']
+    password = request.json['password']
+    firstName = request.json['firstName']
+    lastName = request.json['lastName']
+    birthday = request.json['birthday']
+    response = u.signup(email, username, firstName, lastName, password, birthday)
+    print("Attempted to Signup User %s with status %s", email, response['status_code'])
+    return response
+
+
+@app.route("/healthcheck", methods=["GET"])
+def liveliness():
+    print("SERVER IS RUNNING: ")
+    return '', 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
 
     #
     # front end will hit this endpoint at localhost:port
@@ -41,28 +62,3 @@ def login():
     # else, 404
     # if username == USER: 
     #     print('username is == to user')
-
-@app.route("/signup", methods=["POST"])
-def signup():
-    u = uc.UserController()
-    email = request.json['email']
-    username = request.json['username']
-    password = request.json['password']
-    firstName = request.json['firstName']
-    lastName = request.json['lastName']
-    birthday = request.json['birthday']
-    response = u.signup(email, username, firstName, lastName, password, birthday)
-    return response
-
-
-@app.route("/healthcheck", methods=["GET"])
-def liveliness():
-    print("SERVER IS RUNNING: ")
-    return '', 200
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
-
